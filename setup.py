bb279b33fa5fe74a048d94a939526f7ccce8b987
Jason <j@jsonzilla.com>
2024-07-23 18:33:56 -0300

    setup

--- setup.py
+++ setup.py
@@ -4,21 +4,17 @@ with open(file="README.md", mode="r", encoding="utf-8") as fh:
 
     long_description = fh.read()
 
-setuptools.setup(name='vtt_to_srt3',
-                 version='0.2.0.1',
+setuptools.setup(name='vtt_to_srt2',
+                 version='0.2.0.2',
                  author="Jeison Cardoso",
                  author_email="j@jsonzilla.com",
                  maintainer="Jeison Cardoso",
                  description="vtt to srt subtitles converter package",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 url="https://github.com/jsonzilla/vtt_to_srt3",
+                 url="https://github.com/jsonzilla/vtt_to_srt2",
                  packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
-                 classifiers=["Programming Language :: Python :: 3.7",
-                              "Programming Language :: Python :: 3.8",
-                              "Programming Language :: Python :: 3.9",
-                              "Programming Language :: Python :: 3.10",
-                              "Programming Language :: Python :: 3.11",
+                 classifiers=["Programming Language :: Python :: 2.7",
                               "Operating System :: OS Independent"],
                  entry_points={
                      "console_scripts":
